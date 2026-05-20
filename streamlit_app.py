cc1827106ae7bd0e40b09737f354b2373aa3d1ee
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2026-05-20 18:34:18 +0000

    new 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,148 +1,148 @@
-# import streamlit as st
-# from snowflake.snowpark.functions import col
-# import requests
-# import pandas as pd
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
  
 
  
-# # App Title
-# st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+# App Title
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
  
-# st.write(
-#     """Choose the fruits you want in your custom Smoothie!"""
-# )
+st.write(
+    """Choose the fruits you want in your custom Smoothie!"""
+)
  
-# # Name Input
-# name_on_order = st.text_input('Name on Smoothie:')
-# st.write('The name on your Smoothie will be:', name_on_order)
+# Name Input
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
  
-# # Snowflake Connection
-# cnx = st.connection("snowflake")
-# session = cnx.session()
+# Snowflake Connection
+cnx = st.connection("snowflake")
+session = cnx.session()
  
-# # Get Fruit Data (including the new SEARCH_ON column)
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+# Get Fruit Data (including the new SEARCH_ON column)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
  
-# # Convert the Snowpark Dataframe to a Pandas Dataframe
-# pd_df = my_dataframe.to_pandas()
+# Convert the Snowpark Dataframe to a Pandas Dataframe
+pd_df = my_dataframe.to_pandas()
  
-# # Multiselect using the FRUIT_NAME column from our Pandas Dataframe
-# ingredients_list = st.multiselect(
-#     'Choose up to 5 ingredients:',
-#     pd_df['FRUIT_NAME'],
-#     max_selections=5
-# )
+# Multiselect using the FRUIT_NAME column from our Pandas Dataframe
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    pd_df['FRUIT_NAME'],
+    max_selections=5
+)
  
-# # Insert Order and API Loop
-# if ingredients_list:
-#     ingredients_string = ''
+# Insert Order and API Loop
+if ingredients_list:
+    ingredients_string = ''
  
-#     for fruit_chosen in ingredients_list:
-#         ingredients_string += fruit_chosen + ' '
-#         # Look up the SEARCH_ON value for the chosen fruit
-#         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-#         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-#         st.subheader(fruit_chosen + ' Nutrition Information')
-#         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-#         sf_df = st.dataframe(
-#             data=smoothiefroot_response.json(),
-#             use_container_width=True
-#         )
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        # Look up the SEARCH_ON value for the chosen fruit
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(),
+            use_container_width=True
+        )
  
-#     my_insert_stmt = f"""
-#     insert into smoothies.public.orders
-#     (ingredients, name_on_order)
-#     values
-#     ('{ingredients_string}', '{name_on_order}')
-#     """
+    my_insert_stmt = f"""
+    insert into smoothies.public.orders
+    (ingredients, name_on_order)
+    values
+    ('{ingredients_string}', '{name_on_order}')
+    """
  
-#     time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
  
-#     if time_to_insert:
-#         session.sql(my_insert_stmt).collect()
-#         st.success('Your Smoothie is ordered!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 
 
 
 
     
-# Import python packages.
-import streamlit as st
-import requests
+# # Import python packages.
+# import streamlit as st
+# import requests
 
-from snowflake.snowpark.functions import col
+# from snowflake.snowpark.functions import col
 
-# Write directly to the app.
-cnx = st.connection("snowflake")
+# # Write directly to the app.
+# cnx = st.connection("snowflake")
 
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+# st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
-st.write("Choose the fruits you want in your smoothie")
+# st.write("Choose the fruits you want in your smoothie")
 
-session = cnx.session()
+# session = cnx.session()
 
-name_on_order = st.text_input('Name on Smoothie:')
+# name_on_order = st.text_input('Name on Smoothie:')
 
-st.write('The name on the smoothie will be:', name_on_order)
+# st.write('The name on the smoothie will be:', name_on_order)
 
-my_dataframe = (
-    session.table("smoothies.public.fruit_options")
-    .select(col('FRUIT_NAME'),col('SEARCH_ON'))
-)
+# my_dataframe = (
+#     session.table("smoothies.public.fruit_options")
+#     .select(col('FRUIT_NAME'),col('SEARCH_ON'))
+# )
 
-fruit_rows = my_dataframe.collect()
+# fruit_rows = my_dataframe.collect()
 
-fruit_options = {
-    row["FRUIT_NAME"]: row["SEARCH_ON"]
-    for row in fruit_rows
-}
+# fruit_options = {
+#     row["FRUIT_NAME"]: row["SEARCH_ON"]
+#     for row in fruit_rows
+# }
 
-fruit_list = [row["FRUIT_NAME"] for row in fruit_rows]
+# fruit_list = [row["FRUIT_NAME"] for row in fruit_rows]
 
-ingredients_list = st.multiselect(
-    "Choose upto 5 ingredients",
-    #fruit_list,
-    list(fruit_options.keys()),
-    max_selections=5,
-)
+# ingredients_list = st.multiselect(
+#     "Choose upto 5 ingredients",
+#     #fruit_list,
+#     list(fruit_options.keys()),
+#     max_selections=5,
+# )
 
-if ingredients_list:
+# if ingredients_list:
 
-    ingredients_string = ''
+#     ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
+#     for fruit_chosen in ingredients_list:
 
-        ingredients_string += fruit_chosen + ' '
+#         ingredients_string += fruit_chosen + ' '
 
-        st.subheader(fruit_chosen + ' Nutrition Information')
+#         st.subheader(fruit_chosen + ' Nutrition Information')
 
-        search_on = fruit_options[fruit_chosen]
+#         search_on = fruit_options[fruit_chosen]
 
-        smoothiefroot_response = requests.get(
-            "https://www.smoothiefroot.com/api/fruit/" + search_on
-        )
+#         smoothiefroot_response = requests.get(
+#             "https://www.smoothiefroot.com/api/fruit/" + search_on
+#         )
 
-        st.text(smoothiefroot_response.text)
+#         st.text(smoothiefroot_response.text)
 
-        st.dataframe(
-            data=smoothiefroot_response.json(),
-            use_container_width=True
-        )
+#         st.dataframe(
+#             data=smoothiefroot_response.json(),
+#             use_container_width=True
+#         )
 
-    my_insert_stmt = f"""
-        insert into smoothies.public.orders
-        (ingredients, name_on_order)
-        values
-        ('{ingredients_string}', '{name_on_order}')
-    """
+#     my_insert_stmt = f"""
+#         insert into smoothies.public.orders
+#         (ingredients, name_on_order)
+#         values
+#         ('{ingredients_string}', '{name_on_order}')
+#     """
 
-    time_to_insert = st.button('Submit Order')
+#     time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
+#     if time_to_insert:
 
-        session.sql(my_insert_stmt).collect()
+#         session.sql(my_insert_stmt).collect()
 
-        st.success(
-            f'Your Smoothie is ordered! {name_on_order}!',
-            icon="✅"
-        )
+#         st.success(
+#             f'Your Smoothie is ordered! {name_on_order}!',
+#             icon="✅"
+#         )
