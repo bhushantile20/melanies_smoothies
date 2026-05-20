e1b2c49c524398d1ada8de9d07f4335c9fe01134
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2026-05-20 12:32:48 +0000

    update the the stramlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
+ 
+# App Title
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+ 
+st.write(
+    """Choose the fruits you want in your custom Smoothie!"""
+)
+ 
+# Name Input
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+ 
+# Snowflake Connection
+cnx = st.connection("snowflake")
+session = cnx.session()
+ 
+# Get Fruit Data (including the new SEARCH_ON column)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+ 
+# Convert the Snowpark Dataframe to a Pandas Dataframe
+pd_df = my_dataframe.to_pandas()
+ 
+# Multiselect using the FRUIT_NAME column from our Pandas Dataframe
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    pd_df['FRUIT_NAME'],
+    max_selections=5
+)
+ 
+# Insert Order and API Loop
+if ingredients_list:
+    ingredients_string = ''
+ 
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        # Look up the SEARCH_ON value for the chosen fruit
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(),
+            use_container_width=True
+        )
+ 
+    my_insert_stmt = f"""
+    insert into smoothies.public.orders
+    (ingredients, name_on_order)
+    values
+    ('{ingredients_string}', '{name_on_order}')
+    """
+ 
+    time_to_insert = st.button('Submit Order')
+ 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
